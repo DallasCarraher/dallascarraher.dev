088057bf578c0953e2df8413f6bca7fb21836daa
Dallas Carraher <dall133android@gmail.com>
2020-04-11 12:57:02 -0700

    Adding avatar and sass pkg

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import avatar from "./img/avatar.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={avatar} className="avatar" alt="Dallas" />
         <p>
           <code>This was built using an IDE in the browser :)</code>
         </p>
