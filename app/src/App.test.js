e6784e5e208244daf6a528b6feca4fd043fec9da
Dallas Carraher <dall133android@gmail.com>
2020-04-12 12:31:43 -0700

    Adding husky for pre-commit and pre-push hooks. Also updated tests

--- app/src/App.test.js
+++ app/src/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders app and navbar", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const navbarTitle = getByText(/Dallas Carraher/i);
+  expect(navbarTitle).toBeInTheDocument();
 });
