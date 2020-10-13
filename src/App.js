97afa75ccbfc38c6fbf08f5134e9793d98882f71
DallasCarraher <dallas.carraher@gmail.com>
2020-10-12 23:33:37 -0700

    tons of stuff 🚀

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { Router } from "@reach/router";
 import { ThemeProvider } from "utils";
-import { Home, Resume } from "components";
+import { Home, Resume, Blog } from "components";
 
 function App() {
-  document.title = "dallascarraher.dev";
+  document.title = "Dallas Carraher";
   return (
     <ThemeProvider>
       <Router>
         <Home path="/" />
         <Resume path="/resume" />
+        <Blog path="/blog" />
       </Router>
     </ThemeProvider>
   );
