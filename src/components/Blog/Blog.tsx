ea34cd9bb789fc2b5cc61207bf6d44d1c0bf0d0a
DallasCarraher <dallas.carraher@gmail.com>
2020-12-09 16:06:39 -0800

    cleanup

--- src/components/Blog/Blog.tsx
+++ src/components/Blog/Blog.tsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import { Navigation } from "../Navigation/Navigation";
+import React from "react";
+import { Navigation } from "../common/Navigation/Navigation";
 import { useThemeState, themes } from "utils";
 import "./style.scss";
 
 const { LIGHT } = themes;
 
 export default function Blog() {
-    document.title = "Dallas Carraher | Blog";
-    const { theme } = useThemeState();
-    return (
-        <Navigation>
-            <h2 className={theme === LIGHT ? "light" : "dark"}>Hello, welcome to my blog page. I'm going to do something here soon.</h2>
-        </Navigation>
-    )
+  document.title = "Dallas Carraher | Blog";
+  const { theme } = useThemeState();
+  return (
+    <Navigation>
+      <h2 className={theme === LIGHT ? "light" : "dark"}>
+        Hello, welcome to my blog page. I'm going to do something here soon.
+      </h2>
+    </Navigation>
+  );
 }
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -6,7 +6,7 @@ import BusinessIcon from "@material-ui/icons/Business";
 import LocationOnIcon from "@material-ui/icons/LocationOn";
 
 import { useThemeState, themes } from "utils";
-import { Navigation } from "../Navigation/Navigation";
+import { Navigation } from "../common/Navigation/Navigation";
 import AboutMe from "./AboutMe/AboutMe";
 import avatar from "img/avatar.jpg";
 import "./style.scss";
@@ -22,66 +22,82 @@ export default function Home() {
   const { theme } = useThemeState();
 
   return (
-      <Navigation>
-        <div className="parent-div">
-          <div data-testid="profile" className="profile-div">
-            <Card className={theme === LIGHT ? "card-light" : "card-dark"}>
-              <CardContent>
-                <img src={avatar} alt="Dallas Carraher" className="avatar" />
-                <p> {codeIcon} Front-end Engineer</p>
-                <p> {companyIcon} CDK Global</p>
-                <p> {locationIcon} Hillsboro, OR</p>
-              </CardContent>
-            </Card>
-          </div>
-          <div data-testid="content" className="content-div">
-            <Paper className={theme === LIGHT ? "paper-light" : "paper-dark"}>
-              <AboutMe />
-            </Paper>
-            <Paper className={theme === LIGHT ? "paper-light" : "paper-dark"}>
-              <h2>
-                What I've been up to...{" "}
-                <span role="img" aria-label="thinking-face">
-                  🤔
-                </span>
-              </h2>
-              <p style={{ textAlign: "left", fontSize: "16px" }}>
-                • {" "}
-                <a
-                  href="https://dallascarraher.github.io/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  style={{
-                    fontWeight: "bold",
-                    textDecoration: "none",
-                    color: theme === LIGHT ? "black" : "white",
-                  }}
-                >
-                  markdown <span role="img" aria-label="hand-writing">✍️</span>
-                </a>
-              </p>
-              <p style={{ textAlign: "left", fontSize: "16px" }}>
-                • Learning a lot from{" "}
-                <a
-                  href="https://epicreact.dev/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  style={{
-                    fontWeight: "bold",
-                    textDecoration: "none",
-                    color: theme === LIGHT ? "black" : "white",
-                  }}
-                >
-                  {" "}
-                  Epic React <span role="img" aria-label="atom">⚛️</span>
-                </a>
-              </p>
-              <p style={{ textAlign: "left", fontSize: "16px" }}>
-                • Working on my <Link to="blog" style={{textDecoration: 'none', color: 'black', fontWeight: 'bold'}}>Blog</Link>
-              </p>
-            </Paper>
-          </div>
+    <Navigation>
+      <div className="parent-div">
+        <div data-testid="profile" className="profile-div">
+          <Card className={theme === LIGHT ? "card-light" : "card-dark"}>
+            <CardContent>
+              <img src={avatar} alt="Dallas Carraher" className="avatar" />
+              <p> {codeIcon} Front-end Engineer</p>
+              <p> {companyIcon} CDK Global</p>
+              <p> {locationIcon} Hillsboro, OR</p>
+            </CardContent>
+          </Card>
         </div>
-      </Navigation>
+        <div data-testid="content" className="content-div">
+          <Paper className={theme === LIGHT ? "paper-light" : "paper-dark"}>
+            <AboutMe />
+          </Paper>
+          <Paper className={theme === LIGHT ? "paper-light" : "paper-dark"}>
+            <h2>
+              What I've been up to...{" "}
+              <span role="img" aria-label="thinking-face">
+                🤔
+              </span>
+            </h2>
+            <p style={{ textAlign: "left", fontSize: "16px" }}>
+              •{" "}
+              <a
+                href="https://dallascarraher.github.io/"
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{
+                  fontWeight: "bold",
+                  textDecoration: "none",
+                  color: theme === LIGHT ? "black" : "white",
+                }}
+              >
+                markdown{" "}
+                <span role="img" aria-label="hand-writing">
+                  ✍️
+                </span>
+              </a>
+            </p>
+            <p style={{ textAlign: "left", fontSize: "16px" }}>
+              • Learning a lot from{" "}
+              <a
+                href="https://epicreact.dev/"
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{
+                  fontWeight: "bold",
+                  textDecoration: "none",
+                  color: theme === LIGHT ? "black" : "white",
+                }}
+              >
+                {" "}
+                Epic React{" "}
+                <span role="img" aria-label="atom">
+                  ⚛️
+                </span>
+              </a>
+            </p>
+            <p style={{ textAlign: "left", fontSize: "16px" }}>
+              • Working on my{" "}
+              <Link
+                to="blog"
+                style={{
+                  textDecoration: "none",
+                  color: "black",
+                  fontWeight: "bold",
+                }}
+              >
+                Blog
+              </Link>
+            </p>
+          </Paper>
+        </div>
+      </div>
+    </Navigation>
   );
 }
