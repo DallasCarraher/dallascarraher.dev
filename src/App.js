8ce8bc467192fca64fef16061a324cd4297f4a48
DallasCarraher <dallas.carraher@gmail.com>
2020-12-09 17:02:48 -0800

    theme is working

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
-import React from "react";
-import { Router } from "@reach/router";
-import { ThemeProvider } from "utils";
-import { Home, Resume, Blog } from "components";
+import React from 'react'
+// import { Router } from '@reach/router'
+// import { ThemeProvider } from "utils";
+import { ThemeProvider } from 'styled-components'
+import { darkTheme, lightTheme } from './theme'
+import { GlobalStyles } from './global'
+// import { Home, Resume, Blog } from 'components'
 
 function App() {
-  document.title = "Dallas Carraher";
+  document.title = 'Dallas Carraher'
+  const [theme, setTheme] = React.useState(() => localStorage.getItem('theme'))
+
+  React.useEffect(() => {
+    localStorage.setItem('theme', theme)
+  }, [theme])
+
+  const toggleTheme = () =>
+    theme === 'light' ? setTheme('dark') : setTheme('light')
+
   return (
-    <ThemeProvider>
-      <Router>
-        <Home path="/" />
-        <Resume path="/resume" />
-        <Blog path="/blog" />
-      </Router>
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+      <>
+        <GlobalStyles />
+        <button onClick={toggleTheme}>Toggle theme</button>
+        <h1>
+          {theme === 'light'
+            ? "It's a light theme! 🌞"
+            : "It's a dark theme 🌚"}
+        </h1>
+        <footer></footer>
+      </>
     </ThemeProvider>
-  );
+    // <ThemeProvider>
+    //   <Router>
+    //     <Home path="/" />
+    //     <Resume path="/resume" />
+    //     <Blog path="/blog" />
+    //   </Router>
+    // </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
