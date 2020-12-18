f20b28ea2243dbba28409754d16efbee2d31e186
DallasCarraher <dallas.carraher@gmail.com>
2020-12-17 17:54:38 -0800

    skipppppp

--- src/Views/components/Home.old/__tests__/AboutMe.test.js
+++ src/Views/components/Home.old/__tests__/AboutMe.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { render, within } from '@testing-library/react'
 import AboutMe from '../AboutMe'
 
-test('renders AboutMe Section', () => {
+test.skip('renders AboutMe Section', () => {
   const { getByTestId } = render(<AboutMe />)
   const { getByText } = within(getByTestId('posts'))
   const aboutMeHeader = getByText(/About Me/i)
