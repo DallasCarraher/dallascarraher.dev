a153a24d892e0a435f97546c8a701baee18e2574
DallasCarraher <dallas.carraher@gmail.com>
2020-12-18 21:20:02 -0800

    using styled components more, working on header and nav

--- src/Views/components/Header.jsx
+++ src/Views/components/Header.jsx
@@ -1,12 +1,36 @@
 import React from 'react'
+import styled from 'styled-components'
 import { ThemeIconButton } from 'components/ThemeIconButton'
-import styles from './Header.module.css'
+
+const FlexContainer = styled.header`
+  display: flex;
+  flex-direction: start;
+  justify-content: flex-start;
+  border-bottom: solid 2px ${(props) => props.theme.text};
+`
+
+const MyName = styled.h1`
+  margin-left: 10px;
+  margin-right: 10px;
+  padding: 10px;
+  user-select: none;
+  :hover {
+    cursor: pointer;
+    background-color: rgba(0, 0, 0, 0.07);
+    border-radius: 5px;
+  }
+  :active {
+    cursor: pointer;
+    background-color: rgba(0, 0, 0, 0.2);
+    border-radius: 5px;
+  }
+`
 
 export function Header() {
   return (
-    <header className={styles.flexParent}>
-      <h1 className={styles.flexChild}>Dallas Carraher</h1>
-      <ThemeIconButton className={styles.flexChild} />
-    </header>
+    <FlexContainer>
+      <MyName>Dallas Carraher</MyName>
+      <ThemeIconButton />
+    </FlexContainer>
   )
 }
