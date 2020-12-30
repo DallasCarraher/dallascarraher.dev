79e550824392b97b68ddf52c39d21bba5f335186
DallasCarraher <dallas.carraher@gmail.com>
2020-12-29 16:15:09 -0800

    making good progress today

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -3,11 +3,12 @@ import styled from 'styled-components'
 import { string } from 'prop-types'
 
 const Image = styled.img`
-  box-shadow: 5px 2px 2px 3px rgba(0, 0, 0, 0.1);
-  margin: 10px;
-  height: 200px;
-  width: 200px;
-  border-radius: 100%;
+  /* box-shadow: 5px 2px 2px 3px rgba(0, 0, 0, 0.1); */
+  margin: 20px;
+  height: ${(props) => props.size};
+  width: ${(props) => props.size};
+  border: ${(props) => `solid ${props.theme.text} 4px`};
+  border-radius: 50%;
   user-select: none;
   -moz-user-select: none;
   -webkit-user-drag: none;
@@ -15,12 +16,17 @@ const Image = styled.img`
   -ms-user-select: none;
 `
 
-export const Avatar = ({ src }) => (
+export const Avatar = ({ src, size }) => (
   <span>
-    <Image src={src} />
+    <Image src={src} size={size} />
   </span>
 )
 
 Avatar.propTypes = {
   src: string.isRequired,
+  size: string,
+}
+
+Avatar.defaultProps = {
+  size: '150px',
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,26 +5,19 @@ import { Button } from './Button'
 import { Anchor } from './Anchor'
 import { ThemeIconButton } from './ThemeIconButton'
 
-const StyledHeader = styled.header`
-  position: sticky;
-  top: 0px;
+const StyledNav = styled.nav`
   display: flex;
   flex-direction: row;
-  justify-content: space-between;
+  justify-content: space-around;
   flex-wrap: wrap;
-  padding: 20px;
-  margin: 30px;
+  padding: 10px;
+  margin: 0px;
   align-items: center;
-  /* border-radius: 10px;
-  box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.034),
-    0 6.7px 5.3px rgba(0, 0, 0, 0.048), 0 12.5px 10px rgba(0, 0, 0, 0.06),
-    0 22.3px 17.9px rgba(0, 0, 0, 0.072), 0 1px 33.4px rgba(0, 0, 0, 0.086),
-    0 100px 80px rgba(0, 0, 0, 0.12); */
 `
 
 export const Nav = () => {
   return (
-    <StyledHeader>
+    <StyledNav>
       <Link to="/">
         <Button size="medium">Home</Button>
       </Link>
@@ -40,6 +33,6 @@ export const Nav = () => {
         <Anchor url="https://linkedin.com/in/dallascarraher/" text="LinkedIn" />
       </Button>
       <ThemeIconButton />
-    </StyledHeader>
+    </StyledNav>
   )
 }
