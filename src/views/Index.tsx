7d8998a69af302325e79c9c8de1089721fc33d7b
DallasCarraher <dallas.carraher@gmail.com>
2022-07-09 23:16:18 -0700

    dark background

--- src/views/Index.tsx
+++ src/views/Index.tsx
@@ -6,12 +6,14 @@ export function Index() {
     <>
       <header className="w-full h-96 lt-sm:h-80 bg-cover bg-center bg-no-repeat">
         <div className="max-w-screen-sm h-full mx-auto flex flex-col items-center justify-center">
-          <img
-            src="https://github.com/DallasCarraher.png"
-            alt="A picture of me"
-            className="max-h-56 select-none rounded-full"
-          />
-          <h1 className="text-3xl p-5">Dallas Carraher</h1>
+          <div className="h-56">
+            <img
+              src="https://github.com/DallasCarraher.png"
+              alt="A picture of me"
+              className="h-56 select-none rounded-full"
+            />
+          </div>
+          <h1 className="text-3xl text-white p-5">Dallas Carraher</h1>
         </div>
       </header>
       <main className="max-w-screen-sm px-6 mx-auto"></main>
