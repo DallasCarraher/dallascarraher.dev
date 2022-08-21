d97ec3c14555c460fc1f4a0afdc2d242538a9e4e
DallasCarraher <dallas.carraher@gmail.com>
2022-08-20 23:17:44 -0700

    ts error seems wrong but whatever.

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -4,34 +4,26 @@ export const ProgressBar = ({
 }: {
   bg: string
   percent?: string
-}) => {
-  const containerStyles = {
-    height: 8,
-    width: '30vh',
-    backgroundColor: '#e0e0de',
-    borderRadius: 50,
-  }
-
-  const fillerStyles = {
-    height: '100%',
-    width: `${percent}%`,
-    transition: 'width 1s ease-in-out',
-    backgroundColor: bg,
-    borderRadius: 'inherit',
-    textAlign: 'right',
-  }
-
-  const labelStyles = {
-    padding: 5,
-    color: 'white',
-    fontWeight: 'bold',
-  }
-
-  return (
-    <div style={containerStyles}>
-      <div style={fillerStyles}></div>
-    </div>
-  )
-}
+}) => (
+  <div
+    style={{
+      height: 8,
+      width: '30vh',
+      backgroundColor: '#e0e0de',
+      borderRadius: 50,
+    }}
+  >
+    <div
+      style={{
+        height: '100%',
+        width: `${percent}%`,
+        transition: 'width 1s ease-in-out',
+        backgroundColor: bg,
+        borderRadius: 'inherit',
+        textAlign: 'right',
+      }}
+    ></div>
+  </div>
+)
 
 export default ProgressBar
